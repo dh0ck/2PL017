e56bff7a72caf49cff36e4f7690b2aaf78d5ac41
antlarac <34309036+antlarac@users.noreply.github.com>
2017-12-07 10:31:45 +0100

    Update zpl.py

--- zpl.py
+++ zpl.py
@@ -2,6 +2,27 @@
 
 # 2PL017, by Antonio Lara, 2017
 # Kudos to this guy, I used some of this stuff to start building the GUI: https://www.daniweb.com/programming/software-development/code/216827/working-with-a-tkinter-listbox-python
+'''MIT License
+
+Copyright (c) 2017 antlarac
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.'''
 
 logo = (
 "                                    \n" +
